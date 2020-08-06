ceb297378592b3621a29fa548061d93fa8c3946f
jerry5 <red5trainjerry@gmail.com>
2020-08-06 17:02:26 +0800

    Initial Commit

---
+++ THSRPlus/THSRPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  THSRPlusApp.swift
+//  THSRPlus
+//
+//  Created by jerry cho on 2020/8/6.
+//
+
+import SwiftUI
+
+@main
+struct THSRPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
